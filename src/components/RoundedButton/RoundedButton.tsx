95d76c9c54b5ba1facd9fe380e870d85643ca0b8
Sebastián Silva <sebasilvac88@gmail.com>
2024-04-19 22:25:51 -0400

    basic configuration
     - stackNavigator
     - theme
     - login screen

---
+++ src/components/RoundedButton/RoundedButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { COLORS } from '@/theme/appTheme';
+
+interface RoundedButtonProps {
+  text: string;
+  onPress?: () => void;
+}
+
+export const RoundedButton = (props: RoundedButtonProps) => {
+
+  const { text, onPress } = props;
+
+  return (
+      <TouchableOpacity
+        onPress={onPress}
+        style={styles.roundedButton}
+      >
+        <Text style={styles.textButton}>{text}</Text>
+      </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  roundedButton: {
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: COLORS.primary,
+    padding: 15,
+    borderRadius: 15,
+  },
+  textButton: {
+    color: '#fff',
+  },
+});
